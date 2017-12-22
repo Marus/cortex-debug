88e711f478f81c26bfed66bf9caddcd727542cad
Marcel Ball <me@marcelball.ca>
2017-12-22 12:23:02 -0400

    Added script to generate all the configurationAttributes entries based upon a secondary json file
    Large number of repeated attributes/structures - so wrote a script to update those sections in the package.json file

---
+++ updatePackage.py
@@ -0,0 +1,66 @@
+import json
+import copy
+
+
+config_base = None
+package = None
+
+with open('config_base.json', 'r') as fp:
+	config_base = json.load(fp)
+
+with open('package.json', 'r') as fp:
+	package = json.load(fp)
+
+# print json.dumps(config_base, indent=4)
+
+def make_config(dtype, request):
+	required = copy.deepcopy(config_base['common']['common']['required'])
+	properties = copy.deepcopy(config_base['common']['common']['properties'])
+
+	required_2 = copy.deepcopy(config_base['common'][request]['required'])
+	properties_2 = copy.deepcopy(config_base['common'][request]['properties'])
+
+	required_3 = copy.deepcopy(config_base[dtype]['common']['required'])
+	properties_3 = copy.deepcopy(config_base[dtype]['common']['properties'])
+
+	required_4 = copy.deepcopy(config_base[dtype][request]['required'])
+	properties_4 = copy.deepcopy(config_base[dtype][request]['properties'])
+
+	for r in required_2:
+		if r not in required:
+			required.append(r)
+
+	for r in required_3:
+		if r not in required:
+			required.append(r)
+
+	for r in required_4:
+		if r not in required:
+			required.append(r)
+	
+	properties.update(properties_2)
+	properties.update(properties_3)
+	properties.update(properties_4)
+	
+	return { 'required': required, 'properties': properties }
+
+if 'contributes' in package and 'debuggers' in package['contributes']:
+	for debugger in package['contributes']['debuggers']:
+		dtype = debugger['type']
+		
+		if dtype in config_base:
+			debugger['configurationAttributes'] = {}
+
+			if 'launch' in config_base[dtype]:
+				attrs = make_config(dtype, 'launch')
+				debugger['configurationAttributes']['launch'] = attrs
+			
+			if 'attach' in config_base[dtype]:
+				attrs = make_config(dtype, 'attach')
+				debugger['configurationAttributes']['attach'] = attrs
+	
+# print json.dumps(package, indent=4, sort_keys=True)
+
+with open('package.json', 'w') as fp:
+	json.dump(package, fp, indent=4, sort_keys=True)
+
