1410969af569d4a32196e4c216c8ab0724334976
Haneef Mohammed <haneefdm@gmail.com>
2020-02-08 10:01:15 -0800

    refactorying of unit test

--- test/serialport.test.ts
+++ test/serialport.test.ts
@@ -5,35 +5,34 @@ import * as fs from 'fs';
 import * as vscode from 'vscode';
 
 suite('Serial Port tests', () => {
-    test('Serial Port list', async () => {
-        const extensionPath = vscode.extensions.getExtension('marus25.cortex-debug').extensionPath;
-        const binarypath = path.normalize(path.join(extensionPath, 'binary_modules', process.version,
-            os.platform(), process.arch, 'node_modules'));
-
-        if (!fs.existsSync(binarypath)) {
-            console.error(`Error: Missing Dir '${binarypath}'`);
-            console.error('Try the following commands to create the serial port module:');
-            console.error(`    cd ${extensionPath}`);
-            console.error(`    ./serial-port-build.sh ${process.versions['electron']} ${process.versions.node}`);
-            assert.fail();
-        } else {
-            if (module.paths.indexOf(binarypath) === -1) {
-                module.paths.splice(0, 0, binarypath);
-            }
-
-            let SerialPort;
-            try {
-                SerialPort = module.require('serialport');
-            }
-            catch (e) {
-                assert.fail(e);
-            }
-
-            await SerialPort.list().then((ports) => {
-                for (const port of ports) {
-                    console.log(port);
-                }
-            });
+    const extensionPath = vscode.extensions.getExtension('marus25.cortex-debug').extensionPath;
+    const binaryPath = path.normalize(path.join(extensionPath, 'binary_modules', process.version,
+        os.platform(), process.arch, 'node_modules'));
+    test('Serial Port exists', async () => {
+        if (!fs.existsSync(binaryPath)) {
+            console.error(`Error: Missing dir. '${binaryPath}'`);
+            console.log('Try the following commands to create the serial port module:');
+            console.log(`    cd ${extensionPath}`);
+            console.log(`    ./serial-port-build.sh ${process.versions['electron']} ${process.versions.node}`);
+            assert.fail(`Missing dir ${binaryPath}`);
         }
+    });
+    test('Serial Port list', async () => {
+        if (module.paths.indexOf(binaryPath) === -1) {
+            module.paths.splice(0, 0, binaryPath);
+        }
+        let SerialPort;
+        try {
+            SerialPort = module.require('serialport');
+        }
+        catch (e) {
+            assert.fail(e);
+        }
+
+        await SerialPort.list().then((ports) => {
+            for (const port of ports) {
+                // console.log(port.path);
+            }
+        });
     });
 });
