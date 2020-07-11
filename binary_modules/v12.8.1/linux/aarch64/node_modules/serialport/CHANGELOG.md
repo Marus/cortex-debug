12f37e96f6d89a40cc513c6d5c21930ad292d1df
Marcel Ball <me@marcelball.ca>
2020-07-11 13:35:33 -0300

    Added binary modules to support running on 64-bit Arm using the headmelted community builds of code

---
+++ binary_modules/v12.8.1/linux/aarch64/node_modules/serialport/CHANGELOG.md
@@ -0,0 +1,35 @@
+# Change Log
+
+All notable changes to this project will be documented in this file.
+See [Conventional Commits](https://conventionalcommits.org) for commit guidelines.
+
+# [9.0.0](https://github.com/serialport/node-serialport/compare/v8.0.8...v9.0.0) (2020-05-10)
+
+**Note:** Version bump only for package serialport
+
+
+
+
+
+## [8.0.8](https://github.com/serialport/node-serialport/compare/v8.0.7...v8.0.8) (2020-05-07)
+
+**Note:** Version bump only for package serialport
+
+
+
+
+
+## [8.0.7](https://github.com/serialport/node-serialport/compare/v8.0.6...v8.0.7) (2020-01-30)
+
+**Note:** Version bump only for package serialport
+
+
+
+
+
+## [8.0.6](https://github.com/serialport/node-serialport/compare/v8.0.5...v8.0.6) (2019-12-25)
+
+
+### Bug Fixes
+
+* stream required bindings-mock as a dep ([#1970](https://github.com/serialport/node-serialport/issues/1970)) ([e978b7e](https://github.com/serialport/node-serialport/commit/e978b7eb244b87a6be2ae914965adeb1f4562935))
--- serial-port-build.sh
+++ serial-port-build.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-if [[ $# -ne 2 ]] ; then
-    echo "Usage: $0 <electron-version> <node-version>"
+if [[ $# -ne 3 ]] ; then
+    echo "Usage: $0 <electron-version> <node-version> <architecture>"
     exit 1
 fi
 
 version=$1
 electron=electron@$version
-npm view $electron | grep \\.tarball
+npm view $electron | grep tarball
 if [[ $? -ne 0 ]] ; then
     echo "Error: electron version $electron does not exist"
     exit 1
@@ -15,11 +15,13 @@ fi
 
 nodever=$2
 node=node@$nodever
-npm view $node | grep \\.tarball
+npm view $node | grep tarball
 if [[ $? -ne 0 ]] ; then
     echo "Error: node version $node does not exist"
     exit 1
 fi
+
+architecture=$3
 
 if [[ ! -d ./binary_modules ]] ; then
     # Chances are you are not in the root directory of cortex-debug
@@ -66,14 +68,11 @@ function generate () {
 
 os=`uname`
 if [[ "$os" == 'Linux' ]] ; then
-    generate $version x64 linux
-    # 32-bit linux is dead for VSCode
-    # generate $version x86 linux
+    generate $version $architecture linux
 elif [[ "$os" == 'Darwin' ]] ; then
-    generate $version x64 darwin
+    generate $version $architecture darwin
 else
-    # TODO: yeah, I should check for exact type of env. Just assuming Windows 10 64 bit for now
     echo "Assuming Window 64 bit..."
-    generate $version x64 win32
-    generate $version ia32 win32
+    generate $version $architecture win32
+    generate $version $architecture win32
 fi
