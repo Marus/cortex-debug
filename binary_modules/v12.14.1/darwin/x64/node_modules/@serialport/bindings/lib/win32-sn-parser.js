2384673165b2a5a9f1bdcb2973b446b7bf1dbd0a
Marcel Ball <me@marcelball.ca>
2021-01-21 11:58:51 -0400

    Added updated binary modules for macOS x64

---
+++ binary_modules/v12.14.1/darwin/x64/node_modules/@serialport/bindings/lib/win32-sn-parser.js
@@ -0,0 +1,14 @@
+const PARSERS = [/USB\\(?:.+)\\(.+)/, /FTDIBUS\\(?:.+)\+(.+?)A?\\.+/]
+
+module.exports = pnpId => {
+  if (!pnpId) {
+    return null
+  }
+  for (const parser of PARSERS) {
+    const sn = pnpId.match(parser)
+    if (sn) {
+      return sn[1]
+    }
+  }
+  return null
+}
