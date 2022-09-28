eb3846a987e9f6d36e0a8275e90e1b97988f2e82
Ben McMorran <bemcmorr@microsoft.com>
2022-09-28 11:02:31 -0700

    Return memory references for `void *` variables

--- src/backend/backend.ts
+++ src/backend/backend.ts
@@ -155,7 +155,8 @@ export class VariableObject {
     }
 
     private tryAddMemoryReference(result: object): void {
-        if ((this.numchild > 0) && this.value.startsWith('0x')) {
+        if ((this.numchild > 0 || this.type === "void *")
+            && this.value.startsWith('0x')) {
             result['memoryReference'] = hexFormat(parseInt(this.value));
         }
     }
