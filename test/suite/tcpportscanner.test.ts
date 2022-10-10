d9d23dd444d871dd13c7cb844adf6fe4ac1ca12a
Haneef Mohammed <haneefdm@gmail.com>
2022-10-10 10:17:56 -0700

    upgrade node 16.x

--- test/suite/tcpportscanner.test.ts
+++ test/suite/tcpportscanner.test.ts
@@ -16,7 +16,7 @@ import { TcpPortScanner } from '../../src/tcpportscanner';
 suite('TcpPortScanner Tests', () => {
     test('TcpPortScanner finder/waitfor(open/close) tests', async () => {
         let hrStart = process.hrtime();
-        function timeit(reset: boolean = false) {
+        function timeIt(reset: boolean = false): string {
             const hrEnd = process.hrtime(hrStart);
             const ms = (hrEnd[1] / 1e6).toFixed(2);
             const ret = `${hrEnd[0]}s ${ms}ms`;
@@ -35,36 +35,35 @@ suite('TcpPortScanner Tests', () => {
         };
         let ports: number[];
         const hostNameOrIp = '0.0.0.0';
-        timeit();
+        timeIt();
         await TcpPortScanner.findFreePorts(args, hostNameOrIp).then((ret) => {
-            if (doLog) { console.log(`Found free ports ${ret}, ${timeit()}`); }
+            if (doLog) { console.log(`Found free ports ${ret}, ${timeIt()}`); }
             ports = ret;
             assert.strictEqual(ports.length, args.retrieve, `wrong number of ports ${ports}`);
             assert.strictEqual(ports[0] >= args.min, true);
             assert.strictEqual(ports[args.retrieve - 1] <= args.max, true);
             assert.deepStrictEqual(ports, ports.sort(), `ports are not ordered? ${ports}`);
         }).catch((e) => {
-            assert.fail(`TcpPortScanner.find failed, ${timeit()} ` + e);
+            assert.fail(`TcpPortScanner.find failed, ${timeIt()} ` + e);
         });
 
         const port = ports[1];
-        timeit();
+        timeIt();
         await TcpPortScanner.waitForPortOpen(port, hostNameOrIp, false, 100, 100).then(() => {
-            assert.fail(`0: Timeout expected on port ${port} ${timeit()}`);
+            assert.fail(`0: Timeout expected on port ${port} ${timeIt()}`);
         }, async (err) => {
-            if (doLog) { console.log(`0: Timeout: Success waiting on port ${port} ${timeit()} `, err.message); }
+            if (doLog) { console.log(`0: Timeout: Success waiting on port ${port} ${timeIt()} `, err.message); }
             assert.strictEqual(err.message, 'timeout');
 
             // Lets create a server, but don't start listening for a while. In the meantime, we start looking for
             // ports to get open
             const server = http.createServer();
+            server.on('error', (err: any) => {
+                assert.fail(`Could not start http server on port ${port}`);
+            })
             setTimeout(() => {
-                server.listen(port, (err) => {
-                    if (err) {
-                        assert.fail(`Could not start http server on port ${port}`);
-                    } else {
-                        if (doLog) { console.log(`Http server is listening on ${port}`); }
-                    }
+                server.listen(port, () => {
+                    if (doLog) { console.log(`Http server is listening on ${port}`); }
                 });
             }, 200);            // Enough time to get waitForPortOpen to get started and working
             if (doLog) { console.log('Waiting for http server to start...'); }
@@ -72,24 +71,24 @@ suite('TcpPortScanner Tests', () => {
             // See if the server started on the requested port. We do it two ways in (near) parallel
             // Both should succeed with the same timeout. See above when LISTEN starts
             TcpPortScanner.waitForPortOpen(port, hostNameOrIp, true, 50, 1000).then(async () => {
-                if (doLog) { console.log(`1. Success server port ${port} is ready ${timeit()}`); }
+                if (doLog) { console.log(`1. Success server port ${port} is ready ${timeIt()}`); }
             }, (err) => {
-                if (doLog) { console.log(`1. Timeout: Failed waiting on port ${port} to open ${timeit()}`, err); }
+                if (doLog) { console.log(`1. Timeout: Failed waiting on port ${port} to open ${timeIt()}`, err); }
                 assert.fail('unexpected timeout ' + err);
             });
             await TcpPortScanner.waitForPortOpenOSUtl(port, 50, 1000, false, doLog).then(() => {
-                if (doLog) { console.log(`1.1 Success server port ${port} is ready ${timeit()}`); }
+                if (doLog) { console.log(`1.1 Success server port ${port} is ready ${timeIt()}`); }
             }, (err) => {
-                if (doLog) { console.log(`1.1 Timeout: Failed waiting on port ${port} to open ${timeit()}`, err); }
+                if (doLog) { console.log(`1.1 Timeout: Failed waiting on port ${port} to open ${timeIt()}`, err); }
                 assert.fail('unexpected timeout ' + err);
             });
 
             // Lets see if consecutive ports request works while server is still running. It should
             // skip the port we are already using
             args.consecutive = true;
-            timeit(true);
+            timeIt(true);
             await TcpPortScanner.findFreePorts(args, hostNameOrIp).then((ret) => {
-                if (doLog) { console.log(`Found free consecutive ports ${ret} ${timeit()}`); }
+                if (doLog) { console.log(`Found free consecutive ports ${ret} ${timeIt()}`); }
                 const newPorts = ret;
                 assert.strictEqual(newPorts.length, args.retrieve, `wrong number of ports ${newPorts}`);
                 assert.strictEqual(newPorts[0] >= args.min, true);
@@ -100,15 +99,15 @@ suite('TcpPortScanner Tests', () => {
                     assert.strictEqual(newPorts[ix - 1] + 1, newPorts[ix], `ports are not consecutive ${newPorts}`);
                 }
             }).catch((e) => {
-                assert.fail(`TcpPortScanner.find consecutive failed ${timeit()} ` + e);
+                assert.fail(`TcpPortScanner.find consecutive failed ${timeIt()} ` + e);
             });
 
             server.close();
-            timeit();
+            timeIt();
             await TcpPortScanner.waitForPortClosed(port, hostNameOrIp, true, 50, 1000).then(() => {
-                if (doLog) { console.log(`2. Success Server port ${port} is closed ${timeit()}`); }
+                if (doLog) { console.log(`2. Success Server port ${port} is closed ${timeIt()}`); }
             }, (err) => {
-                if (doLog) { console.log(`2. Timeout: Failed waiting on port ${port} to close ${timeit()}`, err); }
+                if (doLog) { console.log(`2. Timeout: Failed waiting on port ${port} to close ${timeIt()}`, err); }
                 assert.strictEqual(err.message, 'timeout');
                 assert.fail('Why is the server still running? ' + err);
             });
