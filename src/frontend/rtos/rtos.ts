7910bac2aafc78eb02b936ef8837da7d890009fe
VR <vrepetenko@gmail.com>
2022-10-14 15:11:16 +0300

    Feature/chibios-support (#757)
    * Added initial ChibiOS support
    
    * Added stack info, updated thread info
    
    * fixed lint problems

--- src/frontend/rtos/rtos.ts
+++ src/frontend/rtos/rtos.ts
@@ -7,11 +7,13 @@ import * as RTOSCommon from './rtos-common';
 import { RTOSFreeRTOS } from './rtos-freertos';
 import { RTOSUCOS2 } from './rtos-ucosii';
 import { RTOSEmbOS } from './rtos-embos';
+import { RTOSChibiOS } from './rtos-chibios';
 
 const RTOS_TYPES = {
     'FreeRTOS': RTOSFreeRTOS,
     'uC/OS-II': RTOSUCOS2,
-    'embOS': RTOSEmbOS
+    'embOS': RTOSEmbOS,
+    'ChibiOS': RTOSChibiOS
 };
 export class RTOSSession {
     public lastFrameId: number;
