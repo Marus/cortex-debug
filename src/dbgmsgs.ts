a20a149815fd43716b482469037a1378a13948db
Haneef Mohammed <haneefdm@gmail.com>
2025-03-27 15:52:47 -0400

    Add a new debug output channel for future messaging/logging

---
+++ src/dbgmsgs.ts
@@ -0,0 +1,25 @@
+import * as vscode from 'vscode';
+import { HrTimer } from './common';
+
+export class CortexDebugChannel {
+    private static vscodeDebugChannel: vscode.OutputChannel;
+    private static globalHrTimer = new HrTimer();
+
+    public static createDebugChanne() {
+        if (!CortexDebugChannel.vscodeDebugChannel) {
+            const options: object = {
+                log: true
+            };
+            // (options as any).loglevel = vscode.LogLevel.Trace;
+            CortexDebugChannel.vscodeDebugChannel = vscode.window.createOutputChannel('Cortex-Debug');
+            CortexDebugChannel.vscodeDebugChannel.show(true);
+        }
+    }
+
+    public static debugMessage(msg: string): void {
+        if (CortexDebugChannel.vscodeDebugChannel) {
+            const ts = CortexDebugChannel.globalHrTimer.createDateTimestamp();
+            CortexDebugChannel.vscodeDebugChannel.appendLine(ts + ' ' + msg);
+        }
+    }
+}
